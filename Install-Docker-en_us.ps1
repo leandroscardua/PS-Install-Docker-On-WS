6f8c1fbee8600461db057d0320d62bcac547bc8e
Leandro Scardua <leandroscar@gmail.com>
2019-07-14 18:41:05 -0400

    Update Install-Docker-en_us.ps1

--- Install-Docker-en_us.ps1
+++ Install-Docker-en_us.ps1
@@ -1,15 +1,20 @@
-# Atualizar o NuGet para poder baixar a ultima versao do Docker.
+#Requires -RunAsAdministrator
+# Update the NuGet Module
+Write-Host "Atualizando o Gerenciador de Pacotes Nuget"
 Install-PackageProvider -Name NuGet -Force
 
 # Install the Windows Feature Containers on the Server
+Write-Host "Installing Windows Containers Feature"
 Install-WindowsFeature -Name Containers
 
 # Install the Docker Engine Enterprise on the Server
+Write-Host "Installing Docker Engine Enterprise"
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 
-# Install a client interface to manage the Docker Engine Enterprise
+# Install the client interface to manage the Docker Engine Enterprise
+Write-Host "Installing Interface de Gerenciamento of the Docker"
 Install-Package -Name docker -ProviderName DockerMsftProvider
 
-
 # A reboot is required to complete the Docker Installation 
-Restart-Computer -Force
+Write-Host "The server will reboot in 10 Seconds"
+Restart-Computer -Delay 10 -Force
