1f556359fa8db561907b80938325736cce98c5e5
Leandro Scardua <leandroscar@gmail.com>
2019-07-14 18:37:43 -0400

    Update Install-Docker-pt_br.ps1

--- Install-Docker-pt_br.ps1
+++ Install-Docker-pt_br.ps1
@@ -1,14 +1,20 @@
-# Atualizar o NuGet para poder baixar a ultima versao do Docker.
+#Requires -RunAsAdministrator
+# Atualizar o Gerenciador de Pacotes NuGet para poder baixar a ultima versao do Docker.
+Write-Host "Atualizando o Gerenciador de Pacotes Nuget"
 Install-PackageProvider -Name NuGet -Force
 
 # Instalar a Função de Container requerimento necessário para usar o Docker
+Write-Host "Instalando o Containers Feature"
 Install-WindowsFeature -Name Containers
 
 # Instalar o Docker Engine Enterprise no Servidor
+Write-Host "Instalando o Docker Engine Enterprise"
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 
 # Instalar a interface cliente para gerenciar o Docker Engine Enterprise
+Write-Host "Instalando a Interface de Gerenciamento do Docker"
 Install-Package -Name docker -ProviderName DockerMsftProvider
 
 # Uma reinicialização é requerida, devido a instalação do Windows Feature Containers e alterações pertinentes no sistema.
-Restart-Computer -Force
+Write-Host "O Servidor será reiniciado em 10 Segundos"
+Restart-Computer -Delay 10 -Force
